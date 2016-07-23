708d9577dbd2161e1ca4af650548009a3e4e8bb3
root <root@ip-172-31-13-64.ap-northeast-1.compute.internal>
2016-07-23 08:23:26 +0000

    remove swarm support

--- start_console.sh
+++ start_console.sh
@@ -4,20 +4,8 @@ if [ $# -ne 1 ]; then
     exit
 fi
 
-#callMysqlAddress ()
-#{
-#        echo $(curl -sb -H "Accept: application/json" "http://$2:8500/v1/catalog/service/mysql-cluster") \
-#        | (jq -r ".[] | select(.ServiceID | contains(\"registrator:$1:1186\"))")
-#}
-
-#MGMD=$(callMysqlAddress mysql_mgmd0 $1)
-#MGMIP=$(echo ${MGMD} | jq -r '.ServiceAddress')
 MGMIP=$1
 echo "management node's ip ${MGMIP}"
 docker build -t jojo13572001/mysql-cluster mysql
 
-if [ "${MGMIP}" != "" ]; then
-	docker run -it --rm --name ndb_mgmc jojo13572001/mysql-cluster ndb_mgm ${MGMIP}
-else
-	echo "management nodei's ip is wrong!"
-fi
+docker run -it --rm --name ndb_mgmc jojo13572001/mysql-cluster ndb_mgm ${MGMIP}
